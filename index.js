a6accb1a56353ee334ecb8cfab338f9ea9d02bd5
essleeung <essleeung@gmail.com>
2020-04-13 13:11:02 -0400

    bare bones express and layout set up

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 //required node modules
 let express = require('express')
-let 
+let layouts = require('express-ejs-layouts')
+
+//create an app instance
+let app = express()
+
+//set template lang to EJS
+app.set('view engine', 'ejs')
+
+//tell express to use layouts module
+app.use(layouts)
+
+//setup static folder
+app.use(express.static('static'))
+
+//create a wild card (catch-all)
+app.get('*', (req,res) => {
+    res.render('error')
+})
+
+//port to listen on
+app.listen(3000)
